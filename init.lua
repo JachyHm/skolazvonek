7a3c1ed66015c94a6323ed8bc55de7b8a8de04bb
Jáchym Hurtík <jachym.hurtik@gmail.com>
2018-04-07 01:01:25 +0200

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,52 +1,141 @@
--- -- load credentials, 'SSID' and 'PASSWORD' declared and initialize in there
+-- load credentials, 'SSID' and 'PASSWORD' declared in wificonf and initialize in there
+SSID = "EMtest"
+HESLO = "13znakudlouhe"
 dofile("wificonf.lua")
-selhalaNTP = false
-gpio.mode(8,gpio.OUTPUT)
-gpio.mode(5,gpio.OUTPUT)
-gpio.mode(6,gpio.OUTPUT)
-gpio.write(8, gpio.HIGH)
+gpio.mode(1,gpio.OUTPUT) -- inicializace halvního programu a následně pokud svítí, tak je aktivované ztišení
+gpio.mode(5,gpio.OUTPUT) -- připojení k WiFi síti a následně pokud svítí, tak už se nebude zvonit
+gpio.write(1, gpio.HIGH)
 gpio.write(5, gpio.HIGH)
-gpio.write(6, gpio.LOW)
-function startup()
-    if file.open("init.lua") == nil then
-        print("init.lua deleted or renamed")
-    else
-        file.close("init.lua")
-        -- the actual application is stored in 'application.lua'
-        dofile("zvonek.lua")
-    end
+pendingRequestOnTimeSync = false
+blikej = false
+tz = require('tz')
+tz.setzone('Bratislava')
+-- print(tz.getzones())
+emModeFile = file.open("emergencyMode.stav","r")
+if emModeFile then
+	emergencyMode = emModeFile:readline()
+	if emergencyMode == "true" then
+		emergencyMode = true
+	else
+		emergencyMode = false
+	end
+	emModeFile:close(); emModeFile = nil
+else
+	emergencyMode = false
+	emModeFile = file.open("emergencyMode.stav","w")
+	emModeFile:writeline("false")
+	emModeFile:close(); emModeFile = nil
 end
-
+chybaWiFi = false
+uzJeNTP = false
+function startup()
+	-- the actual application is stored in 'zvonek.lua'
+	gpio.mode(2,gpio.OUTPUT)
+	gpio.write(2, gpio.HIGH)
+	dofile("zvonek.lua")
+end
 print("Probiha pripojovani k siti WiFi...")
-wifi.setmode(wifi.STATION)
-wifi.sta.config(SSID, HESLO)
-wifi.sta.connect() --not necessary because config() uses auto-connect=true by default
-tmr.alarm(2, 1000, 1, function()
-    if wifi.sta.getip() == nil then
-        print("Cekani na odpoved serveru...")
-    else
-        tmr.stop(2)
-		gpio.write(8, gpio.LOW)
-        print("Spojeni navazano, IP adresa: " .. wifi.sta.getip())
-		print("Probiha pripojovani k NTP serveru a synchronizace casu...")
-		sntp.sync({"tik.cesnet.cz","tak.cesnet.cz"},
-		function(sec, usec, server, info)
-			print("Uspesna synchronizace, cas od UNIX epochy:", sec, usec,", pouzity server:", server,", dalsi informace:", info)
-			gpio.write(5, gpio.LOW)
-		end,
-		function()
-			print("Selhala synchronizace s NTP!")
-			selhalaNTP = true
-		end
-		)
-		tmr.alarm(1, 1000, 1, function()
-			if rtctime.get() ~= 0 or selhalaNTP == true then
-				tmr.stop(1)
-				print("Pro preruseni zavadeni systemu stisknete libovolnou klavesu...")
-				tmr.alarm(0, 3000, 0, startup)
-			else
-				print("Cekani na odpoved NTP serveru...")
+wifi.sta.clearconfig()
+wifi.nullmodesleep(false)
+wifi.sta.sleeptype(wifi.NONE_SLEEP)
+wifi.setmode(wifi.STATIONAP,true)
+wifi.setphymode(wifi.PHYMODE_G,true)
+print("SSID: "..SSID.." HESLO: "..HESLO)
+station_cfg={}
+station_cfg.ssid=SSID
+station_cfg.pwd=HESLO
+station_cfg.auto=false
+wifi.sta.config(station_cfg)
+wifi.sta.connect()
+poleAP = {}
+poleAP.ssid = "ZvonekNahraj"
+poleAP.pwd = "asdfasdf"
+poleAP.auth = wifi.WPA2_PSK
+wifi.ap.config(poleAP)
+collectgarbage()
+wifi.ap.setip({ip="10.10.10.0",netmask="255.255.255.0",gateway="10.10.10.10"})
+dhcp_config ={}
+dhcp_config.start = "10.10.10.1"
+wifi.ap.dhcp.config(dhcp_config)
+wifi.ap.dhcp.start()
+print(wifi.ap.getip())
+print("\nAktualni konfigurace:")
+for k,v in pairs(wifi.ap.getconfig(true)) do
+	print(k.." :",v)
+end
+tmr.alarm(0,500,1,function() -- az pokud Hostname == Zvonek
+	if wifi.sta.gethostname() ~= "Zvonek" then
+		wifi.sta.sethostname("Zvonek")
+		print(wifi.sta.gethostname())
+	else
+		tmr.unregister(0)
+		wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, function(T) 
+			if not chybaWiFi then
+				if T.reason == 202 or T.reason == 201 or T.reason == 2 then
+					tmr.alarm(6, 500, 1, function()
+						if wifi.sta.getip() == nil then
+							chybaWiFi = true
+							blikej = true
+							emergencyMode = true
+						else
+							chybaWiFi = false
+							tmr.unregister(6)
+							blikej = false
+							emergencyMode = false
+						end
+					end)
+					tmr.unregister(2)
+					dofile("zvonek.lua")
+				end
 			end
 		end)
-    end
+		tmr.alarm(2, 1000, 1, function()
+			if wifi.sta.getip() == nil then
+				print("Cekani na odpoved serveru... ("..wifi.sta.status()..")")
+				wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, function(T) print(T.reason) end)
+			else
+				gpio.write(5, gpio.LOW)
+				tmr.unregister(2)
+				print("Spojeni navazano, IP adresa: " .. wifi.sta.getip())
+				print("Probiha pripojovani k NTP serveru a synchronizace casu...")
+				tmr.alarm(4,1000,1,function()
+					sntp.sync({"tik.cesnet.cz","tak.cesnet.cz"},
+						function(now)
+							print(now)
+							local tm = rtctime.epoch2cal(now + tz.getoffset(now))
+							-- print("Uspesna synchronizace, cas od UNIX epochy:", sec, usec,", pouzity server:", server,", dalsi informace:", info)
+							tmr.unregister(4)
+							tmr.unregister(6)
+							gpio.write(5, gpio.LOW)
+							gpio.write(1, gpio.HIGH)
+							uzJeNTP = true
+						end,
+						function()
+							print("Selhala synchronizace s NTP!")
+							tmr.alarm(6, 200, 1, function()
+								if citac ~= 1 then
+									gpio.write(5, gpio.HIGH)
+									gpio.write(1, gpio.LOW)
+									citac = 1
+								else
+									gpio.write(5, gpio.LOW)
+									gpio.write(1, gpio.HIGH)
+									citac = 0
+								end
+							end)
+						end
+					)
+				end)
+				tmr.alarm(1, 1000, 1, function()
+					if rtctime.get() ~= 0 and uzJeNTP then
+						tmr.unregister(1)
+						print("Pro preruseni zavadeni systemu stisknete libovolnou klavesu...")
+						tmr.alarm(0, 3000, 0, startup)
+					else
+						print("Cekani na odpoved NTP serveru...")
+					end
+				end)
+			end
+		end)
+	end
 end)
--- zvonek.lua
+++ zvonek.lua
@@ -1,149 +1,326 @@
 zvoneni=0
+delkazvoneni = 0
+dnyplatnosti = ""
 buff=""
 pismenko=""
 nejblizsizvoncas=0
 nejblizsizvondelka=0
 aktualnicas=0
-casraw = 0
-cas = 0
-i = 0
-zvstart = ""
-wifistart = ""
+i = 10
+staryDen = ""
 zvoneniobsah = ""
 wifiobsah = ""
-gpio2stare=0
-gpio.mode(1,gpio.OUTPUT)
-gpio.mode(2,gpio.INPUT)
-gpio.mode(3,gpio.INPUT)
+gpio.mode(6,gpio.INPUT)
 gpio.mode(7,gpio.INPUT)
-gpio.write(6, gpio.HIGH)
-gpio.write(1, gpio.LOW)
+gpio.write(2, gpio.LOW)
 byloposlednizvoneni = false
-print("Overuji integritu souboru")
-print("Ukoncuji procesy z minule relace")
-print("Zavadim system")
-casraw = rtctime.epoch2cal(rtctime.get())
-aktualnicas = tonumber(string.format("%02d%02d%02d", casraw["hour"], casraw["min"], casraw["sec"]))+10000
+uzBlika = false
+bylCas = false
+plati = false
+buffZprava = ""
 poslednicheck=aktualnicas
---tady zapíšu aktualnicas do RTC modulu
---porovnám soubor zvoneni.txt se serverem
--- if chcksum(souborserver) != chcksum(souborlokalni) then
-	--stáhni soubor se zvoněním ze serveru
--- end
-wifi.sta.sethostname("Zvonek")
 nodeSRV=net.createServer(net.TCP)
+collectgarbage()
 nodeSRV:listen(59460, function(conn)
-    conn:on("receive", function(conn, receivedData)
-		zvstart = string.find(receivedData, "z:")
-		wifistart = string.find(receivedData, "w:")
-		zvoneniobsah = string.sub(receivedData, zvstart+2, wifistart-1)
-		wifiobsah = string.sub(receivedData, wifistart+2, string.len(receivedData))
-		file.open("zvoneni.txt","w+")
-		file.write(zvoneniobsah)
-		file.close("zvoneni.txt")
-		file.open("wificonf.lua","w+")
-		file.write(wifiobsah)
-		file.close("wificonf.lua")
-		node.restart()
-    end) 
-    conn:on("sent", function(conn) 
-      collectgarbage()
+	conn:on("receive", function(sck, receivedData)
+		-- if string.find(receivedData, "zapnizvon") ~= nil then
+		-- 	zadostZvonSW = true
+		-- elseif string.find(receivedData, "vypnizvon") ~= nil then
+		-- 	zadostZvonSW = false
+		-- elseif string.find(receivedData, "restart") ~= nil then
+		-- 	node.restart()
+		buffZprava = buffZprava..receivedData
+		if string.find(buffZprava, "konec") ~= nil then
+			celaZprava = buffZprava
+			buffZprava = ""
+			if string.find(celaZprava, "dataOK") ~= nil then
+				file.open("wificonf.lua","w+")
+				file.write(wifiobsah)
+				file.close("wificonf.lua")
+				wifiobsah = nil
+				SSIDstare = SSID
+				HESLOstare = HESLO 
+				dofile("wificonf.lua")
+				file.open("zvoneni.txt","w+")
+				file.write(zvoneniobsah)
+				file.close("zvoneni.txt")
+				zvoneniobsah = nil
+				sck:send("UlozenoOK!konec")
+				sck:on("sent", function()
+					blikej = false
+					tmr.unregister(6)
+					if not emergencyMode and SSID == SSIDstare and HESLO == HESLOstare then
+						nejblizsizvoncas,nejblizsizvondelka = nejblizsizvon()
+					else
+						node.restart()
+					end
+				end)
+			else
+				zvstart = string.find(celaZprava, "z:")
+				wifistart = string.find(celaZprava, "w:")
+				if zvstart ~= nil and wifistart ~= nil then
+					zvoneniobsah = string.sub(celaZprava, zvstart+2, wifistart-1)
+					wifiobsah = string.sub(celaZprava, wifistart+2, string.len(celaZprava)-5)
+					
+					response = {}
+					while string.len(celaZprava) > 255 do
+						response[#response + 1] = string.sub( celaZprava, 0, 255)
+						celaZprava = string.sub(celaZprava, 256)
+					end
+					response[#response + 1] = celaZprava
+					local function send(sk)
+						if #response > 0 then
+							sk:send(table.remove(response, 1))
+						else
+							response = nil
+						end
+					end
+					sck:on("sent", send)
+					send(sck)
+
+
+					celaZprava = ""
+				end
+			end
+		end
     end)
 end)
-print("Aktualni cas je:"..aktualnicas)
-function nejblizsizvon()
-	byloposlednizvoneni = false
-	zvonenisoubor = file.open("zvoneni.txt","r")
-	while true do
-		while true do
-			pismenko = zvonenisoubor:read(1)
-			if pismenko == " " then
-				zvoneni = tonumber(buff)
-				buff = ""
-			elseif pismenko == "\n" or pismenko == nil then
-				delkazvoneni = tonumber(buff)
-				buff = ""
-				break
-			else
-				buff = buff..pismenko
-			end
-		end
-		print("Cas zvoneni "..tostring(zvoneni))
-		print("Delka zvoneni "..tostring(delkazvoneni))
-		print("Aktualni cas "..tostring(aktualnicas))
-		if zvoneni > aktualnicas or pismenko == nil then
-			zvonenisoubor:close()
-			if zvoneni < aktualnicas then	
-				zvonenisoubor:close()
-				byloposlednizvoneni = true
-				buff = ""
-				zvonenisoubor = file.open("zvoneni.txt","r")
-			end
-			print("Dosahl jsi konce seznamu "..tostring(byloposlednizvoneni))
-			return zvoneni,delkazvoneni
-		end
+
+function PlatiDnes(t)
+	print(t)
+	platnost = {}
+	platnost[1] = string.sub(t,1,1)
+	platnost[2] = string.sub(t,2,2)
+	platnost[3] = string.sub(t,3,3)
+	platnost[4] = string.sub(t,4,4)
+	platnost[5] = string.sub(t,5,5)
+	platnost[6] = string.sub(t,6,6)
+	platnost[7] = string.sub(t,7,7)
+	
+	tObject = rtctime.epoch2cal(rtctime.get() + tz.getoffset(rtctime.get()))
+	den = tObject["wday"] - 1
+	if den == 0 then
+		den = 7
+	end
+	if platnost[tonumber(den)] == "1" then
+		return true
+	else
+		return false
 	end
 end
-nejblizsizvoncas,nejblizsizvondelka = nejblizsizvon()
-poslednicheck = aktualnicas
-tmr.alarm(3,1000,1,function()
-	casraw = rtctime.epoch2cal(rtctime.get())
-	if i >= 10 then
-		i = 0
-		print(string.format("%04d/%02d/%02d %02d:%02d:%02d", casraw["year"], casraw["mon"], casraw["day"], casraw["hour"], casraw["min"], casraw["sec"]))
-		print(nejblizsizvoncas)
-		print(aktualnicas)
-		print(nejblizsizvondelka)
-	end
-	i = i + 1
-	aktualnicas = tonumber(string.format("%02d%02d%02d", casraw["hour"], casraw["min"], casraw["sec"]))+10000
-	if aktualnicas ~= "" or aktualnicas ~= nil then
-		if nejblizsizvoncas <= aktualnicas then
-			if nejblizsizvoncas + nejblizsizvondelka > aktualnicas then
-				if gpio.read(3) == 0 then
-					gpio.write(1, gpio.HIGH)
-					print("Cingilingi")
+	
+if not emergencyMode then
+	casraw = rtctime.epoch2cal(rtctime.get() + tz.getoffset(rtctime.get()))
+	aktualnicas = tonumber(string.format("%02d%02d%02d", casraw["hour"], casraw["min"], casraw["sec"]))
+	function nejblizsizvon()
+		emergencyMode = true
+		emModeFile = file.open("emergencyMode.stav","w")
+		emModeFile:writeline("true")
+		byloposlednizvoneni = false
+		zvonenisoubor = file.open("zvoneni.txt","r")
+		while true do
+			while true do
+				pismenko = zvonenisoubor:read(1)
+				if pismenko == " " then
+					if not bylCas then
+						zvoneni = tonumber(buff)
+						bylCas = true
+					else
+						bylCas = false
+						delkazvoneni = tonumber(buff)
+					end
+					buff = ""
+				elseif pismenko == "\n" or pismenko == nil then
+					dnyplatnosti = buff
+					buff = ""
+					break
+				else
+					buff = buff..pismenko
 				end
-			else
-				if gpio.read(2) ~= 1 then
-					gpio.write(1, gpio.LOW)
+			end
+			plati = PlatiDnes(dnyplatnosti)
+			print("Plati dnes "..tostring(plati))
+			print("Cas zvoneni "..tostring(zvoneni))
+			print("Delka zvoneni "..tostring(delkazvoneni))
+			print("Aktualni cas "..tostring(aktualnicas))
+			print("Emergency mod "..tostring(emergencyMode))
+			if (zvoneni > aktualnicas and plati) or pismenko == nil then
+				zvonenisoubor:close()
+				if zvoneni < aktualnicas or not plati then
+					byloposlednizvoneni = true
+
+					-- cfg={}
+					-- local casDoPulnoci = 240000 - aktualnicas
+					-- casDoPulnoci = tostring(casDoPulnoci)
+					-- local sekundyDoPulnoci = tonumber(string.sub(casDoPulnoci, -2))
+					-- local minutyDoPulnoci = tonumber(string.sub(casDoPulnoci, -4, -3))
+					-- local hodinyDoPulnoci = tonumber(string.sub(casDoPulnoci, -6, -5))
+					-- cfg.duration=math.min(268435454,((sekundyDoPulnoci*1000)+(minutyDoPulnoci*60000)+(hodinyDoPulnoci*3600000)))
+					-- print("Jdu spat na: "..hodinyDoPulnoci.." hodin, "..minutyDoPulnoci.." minut, "..sekundyDoPulnoci.." sekund. Papa.")
+					-- node.sleep(cfg)
+
+					buff = ""
+					zvonenisoubor = file.open("zvoneni.txt","r")
 				end
-				if byloposlednizvoneni ~= true then
-					nejblizsizvoncas,nejblizsizvondelka = nejblizsizvon()
+				print("Dosahl jsi konce seznamu "..tostring(byloposlednizvoneni))
+				if zvoneni ~= nil and delkazvoneni ~= nil then
+					emergencyMode = false
+					emModeFile:writeline("false")
+					emModeFile:close(); emModeFile = nil
+					return zvoneni,delkazvoneni
+				else
+					blikej = true
+					tmr.unregister(4)
+					tmr.alarm(6, 200, 1, function()
+						if citac ~= 1 then
+							gpio.write(5, gpio.HIGH)
+							gpio.write(1, gpio.HIGH)
+							citac = 1
+						else
+							gpio.write(5, gpio.LOW)
+							gpio.write(1, gpio.LOW)
+							citac = 0
+						end
+					end)
+					return "",""
 				end
 			end
 		end
-		if math.abs(tonumber(aktualnicas) - tonumber(poslednicheck)) > 120000 then
-			sntp.sync({"tik.cesnet.cz","tak.cesnet.cz"},
-				function(sec, usec, server, info)
-					print("Uspesna synchronizace, cas od UNIX epochy:", sec, usec,", pouzity server:", server,", dalsi informace:", info)
-					poslednicheck = aktualnicas
-					gpio.write(5, gpio.LOW)
-				end,
-				function()
-					print("Selhala synchronizace s NTP!")
-					gpio.write(5, gpio.HIGH)
-				end
-			)
+	end
+	function stop() zastavprogram = true end
+	poslednicheck = aktualnicas
+	tmr.alarm(4,1000,1,function()
+		if zastavprogram then tmr.stop(4) end
+		casraw = rtctime.epoch2cal(rtctime.get() + tz.getoffset(rtctime.get()))
+		aktualnicas = tonumber(string.format("%02d%02d%02d", casraw["hour"], casraw["min"], casraw["sec"]))
+		if casraw["wday"] ~= staryDen then
+			staryDen = casraw["wday"]
+			byloposlednizvoneni = false
+			nejblizsizvoncas,nejblizsizvondelka = nejblizsizvon()
 		end
-	end
-	if gpio.read(2) == 1 then
-		gpio.write(1, gpio.HIGH)
-	elseif gpio2stare ~= gpio.read(2) then
-		gpio.write(1, gpio.LOW)
-	end
-	if aktualnicas == 0 then
-		byloposlednizvoneni = false
-	end
-	if byloposlednizvoneni == true then
-		gpio.write(5, gpio.HIGH)
-	else
-		gpio.write(5, gpio.LOW)
-	end
-	if gpio.read(7) == 1 then
-		gpio.write(8, gpio.LOW)
-	else
-		gpio.write(8, gpio.HIGH)
-	end
-	gpio2stare = gpio.read(2)
-end)
+		if i >= 10 then
+			i = 0
+			print("Cas dle UNIX: "..string.format("%04d/%02d/%02d %02d:%02d:%02d", casraw["year"], casraw["mon"], casraw["day"], casraw["hour"], casraw["min"], casraw["sec"]))
+			print("Aktualni cas (bere vpotaz daylight saving time a UTC+1): "..aktualnicas)
+			print("Dosahl jsi posledniho zvoneni dne? (pak neplati nasledujici dva radky): "..tostring(byloposlednizvoneni))
+			print("Cas nejblizsiho zvoneni: "..nejblizsizvoncas)
+			print("Delka nejblizsiho zvoneni: "..nejblizsizvondelka)
+			if gpio.read(7) == 1 then gpio7 = false else gpio7 = true end
+			print("Je aktivovane permanentni zvoneni? "..tostring(gpio7))
+			if gpio.read(6) == 1 then gpio1 = false else gpio1 = true end
+			print("Je aktivovane vynucene vypnuti zvoneni? "..tostring(gpio1))
+			print("Posledni kontrola casu z NTP probehla: "..poslednicheck)
+			print("Emergency mod "..tostring(emergencyMode))
+			
+			print("")
+		end
+		i = i + 1
+		if aktualnicas ~= "" or aktualnicas ~= nil then
+			if nejblizsizvoncas <= aktualnicas then
+				if nejblizsizvoncas + nejblizsizvondelka > aktualnicas and not byloposlednizvoneni then
+					if gpio.read(6) == 1 then
+						gpio.write(2, gpio.HIGH)
+						print("Cingilingi")
+					else
+						gpio.write(2, gpio.LOW)
+					end
+				else
+					if gpio.read(7) ~= 0 then
+						gpio.write(2, gpio.LOW)
+					end
+					if not byloposlednizvoneni then
+						nejblizsizvoncas,nejblizsizvondelka = nejblizsizvon()
+					end
+				end
+			end
+			if math.abs(tonumber(aktualnicas) - tonumber(poslednicheck)) > 120000 then
+				pendingRequestOnTimeSync = true
+			end
+		end
+		if pendingRequestOnTimeSync then
+			if wifi.sta.getip() ~= nil and not blokProbiha then
+				blokProbiha = true
+				pendingRequestOnTimeSync = false
+				getDST()
+				sntp.sync({"tik.cesnet.cz","tak.cesnet.cz"},
+					function(sec, usec, server, info)
+						print("Uspesna synchronizace, cas od UNIX epochy:", sec, usec,", pouzity server:", server,", dalsi informace:", info)
+						print("")
+						poslednicheck = aktualnicas
+						tmr.stop(3)
+						blokProbiha = false
+					end,
+					function()
+						print("Selhala synchronizace s NTP!")
+						blokProbiha = false
+						pendingRequestOnTimeSync = true
+						tmr.alarm(3, 200, 1, function()
+							if citac ~= 1 then
+								gpio.write(5, gpio.HIGH)
+								gpio.write(1, gpio.LOW)
+								citac = 1
+							else
+								gpio.write(5, gpio.LOW)
+								gpio.write(1, gpio.HIGH)
+								citac = 0
+							end
+						end)
+						print("")
+					end
+				)
+			else
+				tmr.alarm(3, 200, 1, function()
+					if citac ~= 1 then
+						gpio.write(5, gpio.HIGH)
+						gpio.write(1, gpio.LOW)
+						citac = 1
+					else
+						gpio.write(5, gpio.LOW)
+						gpio.write(1, gpio.HIGH)
+						citac = 0
+					end
+				end)
+			end
+		end
+		if not blikej then
+			if byloposlednizvoneni then
+				gpio.write(5, gpio.HIGH)
+			else
+				gpio.write(5, gpio.LOW)
+			end
+		end
+		if gpio.read(6) == 0 then
+			gpio.write(2, gpio.LOW)
+			if not blikej then
+				gpio.write(1, gpio.HIGH)
+			end
+		else
+			if not blikej then
+				gpio.write(1, gpio.LOW)
+			end
+		end
+		if gpio.read(7) == 0 then
+			gpio.write(2, gpio.HIGH)
+			gpio7stare = 0
+		elseif gpio7stare == 0 then
+			gpio.write(2, gpio.LOW)
+			gpio7stare = 1
+		end
+		if blikej and not uzBlika then
+			uzBlika = true
+			tmr.alarm(5,200,1,function()
+				if blikej and citac ~= 1 then
+					gpio.write(5, gpio.HIGH)
+					gpio.write(1, gpio.HIGH)
+					citac = 1
+				elseif blikej then
+					gpio.write(5, gpio.LOW)
+					gpio.write(1, gpio.LOW)
+					citac = 0
+				end
+			end)
+		elseif not blikej then
+			tmr.unregister(5)
+		end
+	end)
+end
